e1854f84db7b0a1da3fe0485a61edc5660ea7459
jvmdo <deoliveira.jvm@gmail.com>
2023-06-27 12:19:09 -0400

    refactor[hooks/usePagination: remove unnecessary states and side effect

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,39 +1,36 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
-function generateRange(length: number, rowsPerPage: number) {
+function calculatePages(length: number, rowsPerPage: number) {
   return Array.from({ length: Math.ceil(length / rowsPerPage) }, (_, i) => ++i)
 }
 
-function sliceRange(currentPage: number, range: number[], slice = 3) {
-  if (currentPage === range.at(0)! - 1) {
-    return range.slice(0, slice)
-  } else if (currentPage === range.at(-1)! - 1) {
-    return range.slice(-slice, range.length)
+function sliceControls(currentPage: number, pages: number[], range = 3) {
+  if (currentPage === pages[0]) {
+    return pages.slice(0, range)
+  } else if (currentPage >= pages.at(-2)!) {
+    return pages.slice(-range, pages.length)
   }
-  return range.slice(currentPage - 1, currentPage + slice - 1)
+  return pages.slice(currentPage - 1, currentPage + range - 1)
 }
 
 function sliceData<T>(data: T[], page: number, rowsPerPage: number) {
   return data.slice((page - 1) * rowsPerPage, page * rowsPerPage)
 }
 
-export function usePagination<T>(data: T[], page: number, rowsPerPage: number) {
-  const [pageRange, setPageRange] = useState<number[]>([])
-  const [slice, setSlice] = useState<T[]>([])
+export function usePagination<T>(data: T[], rowsPerPage: number) {
+  const [page, setPage] = useState(1)
 
-  useEffect(() => {
-    const range = generateRange(data.length, rowsPerPage)
-    setPageRange([...range])
+  const pages = calculatePages(data.length, rowsPerPage)
 
-    const slice = sliceData<T>(data, page, rowsPerPage)
-    setSlice([...slice])
-
-    // document.querySelector('.main-content')?.scrollTop = 0
-  }, [data, setPageRange, page, setSlice, rowsPerPage])
+  const paginate = sliceData<T>(data, page, rowsPerPage)
+  const controls = sliceControls(page, pages)
+  const controlsLength = pages.length
 
   return {
-    slice,
-    range: sliceRange(page - 1, pageRange),
-    rangeLength: pageRange.length,
+    paginate,
+    page,
+    setPage,
+    controls,
+    controlsLength,
   }
 }
